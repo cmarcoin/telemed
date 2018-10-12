4956b4ca5918617df508ab133298ae3563aa2198
Charles Marcoin <charles.marcoin@gmail.com>
2018-10-12 18:37:33 +0200

    added homepage login buttons and a navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :doctors, controllers: { sessions: 'doctors/sessions' }
+
+  # Using devise with multiple models : https://github.com/plataformatec/devise/wiki/How-to-Setup-Multiple-Devise-User-Models
+  devise_for :doctors, controllers: { sessions: 'doctors/sessions',
+                                      registrations: 'doctors/registrations' }
   devise_for :pharmacists, controllers: { sessions: 'pharmacists/sessions',
                                           registrations: 'pharmacists/registrations' }
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
-
-
-
   namespace :pharmacists do
     get 'accueil', to: 'pages#index'
-
   end
 
 
